68a99f2348fbe547e56a21c7fadb089abd94a9e7
Ronaldo Silva <ronaldo.intermec@gmail.com>
2022-12-27 16:59:56 -0300

    test 1 and 2 ok

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable array-callback-return */
 import React, { useState, useEffect } from 'react';
 
 import income from '../../assets/income.svg';
@@ -30,12 +31,34 @@ interface Balance {
 }
 
 const Dashboard: React.FC = () => {
-  // const [transactions, setTransactions] = useState<Transaction[]>([]);
-  // const [balance, setBalance] = useState<Balance>({} as Balance);
+  const [t, setTransactions] = useState<Transaction[]>([]);
+  const [b, setBalance] = useState<Balance>({} as Balance);
 
   useEffect(() => {
     async function loadTransactions(): Promise<void> {
-      // TODO
+      const response = await api.get('/transactions');
+      const { balance, transactions } = response.data;
+
+      transactions.map((transaction: Transaction) => {
+        // eslint-disable-next-line no-param-reassign
+        transaction.formattedValue =
+          transaction.type === 'income'
+            ? ` ${formatValue(transaction.value)}`
+            : `-  ${formatValue(transaction.value)}`;
+      });
+
+      setTransactions(transactions);
+      const bal: Balance = {
+        income: '',
+        outcome: '',
+        total: '',
+      };
+
+      bal.income = `${formatValue(balance.income)}`;
+      bal.outcome = `${formatValue(balance.outcome)}`;
+      bal.total = `${formatValue(balance.total)}`;
+
+      setBalance(bal);
     }
 
     loadTransactions();
@@ -51,21 +74,21 @@ const Dashboard: React.FC = () => {
               <p>Entradas</p>
               <img src={income} alt="Income" />
             </header>
-            <h1 data-testid="balance-income">R$ 5.000,00</h1>
+            <h1 data-testid="balance-income">{b.income}</h1>
           </Card>
           <Card>
             <header>
               <p>Saídas</p>
               <img src={outcome} alt="Outcome" />
             </header>
-            <h1 data-testid="balance-outcome">R$ 1.000,00</h1>
+            <h1 data-testid="balance-outcome">{b.outcome}</h1>
           </Card>
           <Card total>
             <header>
               <p>Total</p>
               <img src={total} alt="Total" />
             </header>
-            <h1 data-testid="balance-total">R$ 4000,00</h1>
+            <h1 data-testid="balance-total">{b.total}</h1>
           </Card>
         </CardContainer>
 
@@ -80,20 +103,16 @@ const Dashboard: React.FC = () => {
               </tr>
             </thead>
 
-            <tbody>
-              <tr>
-                <td className="title">Computer</td>
-                <td className="income">R$ 5.000,00</td>
-                <td>Sell</td>
-                <td>20/04/2020</td>
-              </tr>
-              <tr>
-                <td className="title">Website Hosting</td>
-                <td className="outcome">- R$ 1.000,00</td>
-                <td>Hosting</td>
-                <td>19/04/2020</td>
-              </tr>
-            </tbody>
+            {t.map(transaction => (
+              <tbody key={transaction.id}>
+                <tr>
+                  <td className="title">{transaction.title}</td>
+                  <td className="income">{transaction.formattedValue}</td>
+                  <td>{transaction.category.title}</td>
+                  <td>{transaction.created_at}</td>
+                </tr>
+              </tbody>
+            ))}
           </table>
         </TableContainer>
       </Container>
--- src/utils/formatValue.ts
+++ src/utils/formatValue.ts
@@ -1,4 +1,14 @@
 const formatValue = (value: number): string =>
-  Intl.NumberFormat().format(value); // TODO
+  new Intl.NumberFormat('pt-BR', {
+    style: 'currency',
+    minimumFractionDigits: 2,
+    currency: 'BRL',
+  }).format(value); // TODO
 
 export default formatValue;
+
+// - R$ 5.500,00
+// const formatValue = (value: number): string =>
+//   Intl.NumberFormat().format(value); // TODO
+
+// export default formatValue;
